eb85af29074fc932edd28bc84624b7e82a5120ae
Miguel Angel <Miguel Angel@LAPTOP-4O0Q885R>
2024-05-09 12:07:58 -0500

    Agregamos logica al controlador de Registro y personalizacion de botones en registro

--- src/controller/RegistroController.java
+++ src/controller/RegistroController.java
@@ -4,12 +4,19 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -35,10 +42,75 @@ public class RegistroController implements Initializable {
 
     @FXML
     private void ShowLogin(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ViewInicio/MiLOGIN.fxml"));
+            Parent root = loader.load();
+            
+            ViewLoginController controller = loader.getController();
+            
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            
+            stage.setScene(scene);
+            stage.show();
+            
+            stage.setOnCloseRequest(e -> controller.CloseWindows());
+            
+            Stage myStage = (Stage) this.btnCrearCuenta.getScene().getWindow();
+            myStage.close();
+                    
+        } catch (IOException ex) {
+            Logger.getLogger(ViweINICOController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @FXML
     private void ShowLogin2(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ViewInicio/MiLOGIN.fxml"));
+            Parent root = loader.load();
+            
+            ViewLoginController controller = loader.getController();
+            
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            
+            stage.setScene(scene);
+            stage.show();
+            
+            stage.setOnCloseRequest(e -> controller.CloseWindows());
+            
+            Stage myStage = (Stage) this.btnLogin.getScene().getWindow();
+            myStage.close();
+                    
+        } catch (IOException ex) {
+            Logger.getLogger(ViweINICOController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @FXML
+    void ShowLogin3(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ViewInicio/MiLOGIN.fxml"));
+            Parent root = loader.load();
+            
+            ViewLoginController controller = loader.getController();
+            
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            
+            stage.setScene(scene);
+            stage.show();
+            
+            stage.setOnCloseRequest(e -> controller.CloseWindows());
+            
+            Stage myStage = (Stage) this.btnVolver.getScene().getWindow();
+            myStage.close();
+                    
+        } catch (IOException ex) {
+            Logger.getLogger(ViweINICOController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
     public void CloseWindows(){
         
