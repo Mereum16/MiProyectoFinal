58458950fe4809d794ef48132e77bb3ef6d1c955
Miguel Angel <Miguel Angel@LAPTOP-4O0Q885R>
2024-05-10 13:57:40 -0500

    Ventana Compra

--- src/controller/CompraController.java
+++ src/controller/CompraController.java
@@ -4,9 +4,19 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,13 +25,41 @@ import javafx.fxml.Initializable;
  */
 public class CompraController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private ImageView Volver;
+
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    @FXML
+    void Inicio(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ViewInicio/PANTALLAINICIO.fxml"));
+            Parent root = loader.load();
+            
+            PANTALLAINICIOController controller = loader.getController();
+            
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            
+            stage.setScene(scene);
+            
+            stage.show();
+            
+            
+            stage.setOnCloseRequest(e -> controller.CloseWindows());
+            
+            Stage myStage = (Stage) this.Volver.getScene().getWindow();
+            
+            myStage.close();
+                    
+        } catch (IOException ex) {
+            Logger.getLogger(PANTALLAINICIOController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
     public void CloseWindows(){
         
     }
--- src/controller/PANTALLAINICIOController.java
+++ src/controller/PANTALLAINICIOController.java
@@ -108,7 +108,7 @@ public class PANTALLAINICIOController implements Initializable {
         
         
         
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        /*throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody*/
     }
     public void CloseWindows(){
         
